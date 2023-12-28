7fb3e04ee053250ead289d4fd7ef2c4cba13e6ad
liuwenjun05 <liuwenjun05@meituan.com>
2023-12-28 20:09:32 +0800

    feat: add route params

---
+++ app/dashboard/page.js
@@ -0,0 +1,13 @@
+/*
+ * @Author: liuwenjun05 liuwenjun05@meituan.com
+ * @Date: 2023-12-28 20:00:06
+ * @LastEditors: liuwenjun05
+ * @Description: file content
+ */
+import React from 'react'
+
+export default function Page() {
+  return (
+    <div>dashboard</div>
+  )
+}
